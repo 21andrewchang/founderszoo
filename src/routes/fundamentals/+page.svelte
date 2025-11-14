a14e9e392024dd0a342d66d39549ca1357f68db1
nicolu0 <nicoluo@gmail.com>
2025-11-14 15:56:40 -0600

    fix: sizing and spacing

--- src/routes/fundamentals/+page.svelte
+++ src/routes/fundamentals/+page.svelte
@@ -43,13 +43,13 @@
     <div class="w-1/3 mx-auto flex flex-col items-center justify-center h-dvh text-justify selection:bg-stone-600 selection:text-stone-100">
         <div class="flex flex-col">
             {#each sections as section}
-                <h1 style="font-family: 'Cormorant Garamond', serif" class="text-stone-700 text-[16px] tracking-wide mb-1 italic">
+                <h1 style="font-family: 'Cormorant Garamond', serif" class="text-stone-700 text-lg tracking-wide mb-1 italic">
                     {section.title}
                 </h1>
 
                 {#each section.paragraphs as paragraph, i}
                     <p
-                        class={`text-stone-600 text-[14px] ${
+                        class={`text-stone-600 text-sm ${
                             i < section.paragraphs.length - 1 ? 'mb-2' : 'mb-5'
                         }`}
                     >
